f4ce49a0b63107e34b6ca70b59e180af803b35f0
osergeiev <os55433@fer.hr>
2025-01-20 01:04:40 +0100

    Env variable creating

--- SourceCode/Backend/src/main/java/com/crisisguard/crisisguard/CrisisguardApplication.java
+++ SourceCode/Backend/src/main/java/com/crisisguard/crisisguard/CrisisguardApplication.java
@@ -1,12 +1,14 @@
 package com.crisisguard.crisisguard;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-public class CrisisguardApplication {
+public class CrisisguardApplication implements CommandLineRunner{
 	public static void main(String[] args) {
 		SpringApplication.run(CrisisguardApplication.class, args);
 //		try {
@@ -25,4 +27,22 @@ public class CrisisguardApplication {
 //	  googleCredentials.refresh();
 //	  return googleCredentials.getAccessToken().getTokenValue();
 //}
+	@Override
+	 public void run(String... args) throws Exception {
+	        String credentialsJson = System.getenv("GOOGLE_CREDENTIALS_JSON");
+
+	        if (credentialsJson != null && !credentialsJson.isEmpty()) {
+	            // Write the credentials to a temporary file on Heroku's filesystem
+	            try (FileOutputStream fos = new FileOutputStream("/tmp/service-account-key.json")) {
+	                fos.write(credentialsJson.getBytes());
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	            }
+
+	            // Set GOOGLE_APPLICATION_CREDENTIALS to the file path
+	            System.setProperty("GOOGLE_APPLICATION_CREDENTIALS", "/tmp/service-account-key.json");
+	        } else {
+	            System.err.println("Google credentials JSON not found in environment variables.");
+	        }
+	    }
 }
